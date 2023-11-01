d34831d81d55602e8f2eeb145ac5a50dff47104b
Upeksha Wijerathne <109344358+Upeksha-Wije@users.noreply.github.com>
2023-11-01 09:10:57 +0530

    Create currency-convertor.py
    Currency convertor simple app using Python

---
+++ currency-convertor.py
@@ -0,0 +1,56 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout
+from PyQt6.QtWidgets import QLabel, QPushButton,QLineEdit, QComboBox
+from bs4 import BeautifulSoup
+import requests
+
+import lxml.etree as etree
+
+
+parser = etree.HTMLParser()
+
+def get_currency(in_currency='USD', out_currency='LKR') :
+    url= f'https://www.x-rates.com/calculator/?from={in_currency}&to={out_currency}&amount=1'
+    content= requests.get(url).text
+    soup = BeautifulSoup(content,'html.parser')
+    rate = soup.find("span", class_="ccOutputRslt").get_text()
+    rate = float(rate[:-4])
+
+    return rate
+
+def show_currency():
+    input_text = float(text.text())
+    rate = get_currency()
+    output = (input_text * rate)
+    output_label.setText(str(output))
+
+app = QApplication([])
+window = QWidget()
+label = QLabel()
+btn = QPushButton()
+window.setWindowTitle('Currency Convertor')
+
+layout = QVBoxLayout()
+
+in_combo = QComboBox()
+currencies = ['USD','EUR', 'LKR']
+in_combo.addItems(currencies)
+layout.addWidget(in_combo)
+
+target_combo = QComboBox()
+target_combo.addItems(currencies)
+layout.addWidget(target_combo)
+
+
+text = QLineEdit()
+layout.addWidget(text)
+
+btn = QPushButton('Convert')
+layout.addWidget(btn)
+btn.clicked.connect(show_currency)
+
+output_label = QLabel('')
+layout.addWidget(output_label)
+
+window.setLayout(layout)
+window.show()
+app.exec()
